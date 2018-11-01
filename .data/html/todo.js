e76b1150da0bebbe0b03fb3b3f669573685ca234
koyata tezuka <koytez@gmail.com>
2018-11-01 11:45:58 -1000

    add plus and minus icon

--- .data/html/todo.js
+++ .data/html/todo.js
@@ -8,6 +8,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css?family=Permanent+Marker" rel="stylesheet">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns" crossorigin="anonymous">
   <link rel="stylesheet" media="screen" href="style.css" />
 </head>
 
