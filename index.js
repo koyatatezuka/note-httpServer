b52c8183ce5d723938e77ad052ab618924f943da
koyata tezuka <koytez@gmail.com>
2019-01-24 14:53:00 -1000

    route testing

--- index.js
+++ index.js
@@ -5,12 +5,11 @@ const server = require('./lib/server');
 const PORT =  process.env.PORT || 3000;
 
 // instatiating http server
-const httpServer = http.createServer((req, res) => {
-  server(req,res)
-});
+const httpServer = http.createServer(server);
 
 // start http server
 httpServer.listen(PORT, () => {
 	console.log(`Listening on PORT: ${PORT}`);
 });
 
+module.exports = httpServer;
